2d4bf022b3973dfd24715c77b5a020cfba428a2c
SassythSasqutch_SPXSQ1 <sassythsasqutch@gmail.com>
2021-08-20 16:52:11 +0100

    Added code to inject bundles, add registries

--- ext/Shared/__init__.lua
+++ ext/Shared/__init__.lua
@@ -2,9 +2,9 @@
 -- Loading assets --
 --------------------
 
-print('Loading assets...')
-
 Events:Subscribe('Level:LoadResources', function() -- Prepare bundles
+
+    print('Mounting superbundles...')
     
      -- Get Osprey bundles
     ResourceManager:MountSuperBundle('spchunks')
@@ -16,21 +16,38 @@ Events:Subscribe('Level:LoadResources', function() -- Prepare bundles
 
 end)
 
--- Note server crash on round restart - maybe unsubscribe to this (problem could also be with Janssent's bicycle)
-
 Hooks:Install('ResourceManager:LoadBundles', 500, function(hook, bundles, compartment) -- Inject bundles
 
     if #bundles == 1 and bundles[1] == ServerUtils:GetLevelName() then
 
+        print('Injecting bundles...')
+
         bundles = {
             'levels/sp_earthquake2/sp_earthquake2',
             'levels/xp1_004/xp1_004',
+            'levels/xp1_004/cq_s',
             bundles[1]
         }
 
+    end
+
+    hook:Pass(bundles, compartment)
+
 end)
 
-print('Assets loaded!')
+-- Add registries
+
+Events:Subscribe('Level:RegisterEntityResources', function(levelData)
+
+    print('Adding Uprising registry...')
+    local uprisingRegistry = RegistryContainer(ResourceManager:FindInstanceByGuid(Guid('6FD69AE4-5B8A-11E0-BC14-D5B461CF665B'), Guid('7C9240B4-8C13-9DCA-B21F-E436E9A79F8F')))
+    ResourceManager:AddRegistry(uprisingRegistry, ResourceCompartment.ResourceCompartment_Game)
+
+    print('Adding Wake Island CQS registry...')
+    local wakeIsCqsRegistry = RegistryContainer(ResourceManager:FindInstanceByGuid(Guid('6C5E1DF6-B428-4A11-BA06-15E565B63802'), Guid('A44167A2-B0D1-3EA5-C4DD-517FD23EC6A2')))
+    ResourceManager:AddRegistry(wakeIsCqsRegistry, ResourceCompartment.ResourceCompartment_Game)
+
+end)
 
 ---------------------
 -- Replacing model --
